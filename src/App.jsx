4cb0473770028c09021cd4b2b47df569f5c5afb1
Guney Unay <unaygney17@gmail.com>
2023-09-16 18:33:51 +0300

    project dependencies loaded

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import MainPage from "./components/MainPage"
+function App() {
+  return (
+    <>
+<MainPage />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+<p className='text-white'>Header Page</p>
+    </div>
+  )
+}
+
+export default Header
